514c839eab4a71b836424891e26a29e8d465f5da
PetrBalkunov <balkunovpetr81@gmail.com>
2023-03-28 16:13:56 +0300

    Проверка

--- LegalPY/Lesson_1/HSE_Balkunov_lesson_1.py
+++ LegalPY/Lesson_1/HSE_Balkunov_lesson_1.py
@@ -9,9 +9,17 @@ print(type(a))
 
 
 
-for i in a:
-    print(i)
+number_1 = int(input('Введите число '))
 
-
-number_1 = input(int(is.digit('Введите число ')))
 print (number_1)
+
+#Решаем 1-ую задачку, когда пользователь вводит число, а это число нужно перевести в секунды, минуты и часы
+
+random_number = int(input('Введите число '))
+while (not random_number.isdigit()):
+    random_number = int(input('Введите число '))
+    print()
+    if not random_number.isdigit()
+        print ('Введите, пожалуйста, любое число, а не текст')
+    else:
+        print('Спасибо! Сейчас мы быстренько все расчитаем:')
